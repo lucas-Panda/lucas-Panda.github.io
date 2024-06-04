8d00fc81873d69c68d4b7bd088ef532960696bb9
lucas-Panda <148153649+lucas-Panda@users.noreply.github.com>
2024-06-03 21:33:12 -0300

    Update addcarrinho.js

--- scripts/addcarrinho.js
+++ scripts/addcarrinho.js
@@ -1,33 +1,41 @@
-// Seleciona todos os botões "Adicionar ao Carrinho"
+// botões "Adicionar ao Carrinho"
 var botoesAdicionar = document.querySelectorAll('.add-to-cart');
+//---------------------------------------------------
 
 // Função para adicionar item ao localStorage
 function adicionarAoCarrinho(nome, preco, imagem) {
-    // Obtém o carrinho do localStorage ou cria um novo array se não existir
+    // Obtém o carrinho do localStorage
     var carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+    //---------------------------------------------------
     
-    // Adiciona o novo item ao carrinho
+    // Adiciona o item ao carrinho
     carrinho.push({ name: nome, price: preco, image: imagem });
+    //---------------------------------------------------
     
-    // Salva o carrinho atualizado no localStorage
+    // atualizado o localStorage
     localStorage.setItem('carrinho', JSON.stringify(carrinho));
+    //---------------------------------------------------
 }
 
 // Adiciona um event listener para cada botão "Adicionar ao Carrinho"
 botoesAdicionar.forEach(function(botao) {
     botao.addEventListener('click', function() {
-        // Obtém a div do item correspondente
+        // pega a div do item 
         var item = botao.closest('.produtos');
+        //------------------------------------
         
-        // Obtém os dados do item
+        // Informações do item
         var itemName = item.getAttribute('data-name');
         var itemPrice = item.getAttribute('data-price');
         var itemImage = item.querySelector('img').src;
+        //---------------------------------------------------
         
-        // Adiciona o item ao carrinho
+        // manda pro carrinho
         adicionarAoCarrinho(itemName, itemPrice, itemImage);
+        //---------------------------------------------------
         
-        // Feedback para o usuário
+        // aviso
         alert(itemName + " foi adicionado ao carrinho.");
+        //---------------------------------------------------
     });
 });
